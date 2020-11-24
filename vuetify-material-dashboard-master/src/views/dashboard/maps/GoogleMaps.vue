1479afbad4eb3584fd8ec4acf728ab21a09c527c
seunghwan <lovelyunsh@naver.com>
2020-11-24 21:46:34 +0900

    지도 검색 위치

--- vuetify-material-dashboard-master/src/views/dashboard/maps/GoogleMaps.vue
+++ vuetify-material-dashboard-master/src/views/dashboard/maps/GoogleMaps.vue
@@ -1,18 +1,23 @@
 <template>
   <v-container id="google-maps" fluid tag="section">
-    <v-text-field
-      :label="$t('search')"
-      v-model="place"
-      color="secondary"
-      hide-details
-      style="max-width: 165px"
-    >
-      <template v-if="$vuetify.breakpoint.mdAndUp" v-slot:append-outer>
-        <v-btn class="mt-n2" @click="changeplace" elevation="1" fab small>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-      </template>
-    </v-text-field>
+    <v-row>
+      <v-col cols="10"></v-col>
+      <v-col>
+        <v-text-field
+          :label="$t('search')"
+          v-model="place"
+          color="secondary"
+          hide-details
+          style="max-width: 165px"
+        >
+          <template v-if="$vuetify.breakpoint.mdAndUp" v-slot:append-outer>
+            <v-btn class="mt-n2" @click="changeplace" elevation="1" fab small>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+          </template>
+        </v-text-field>
+      </v-col>
+    </v-row>
     <v-row>
       <v-col cols="12">
         <base-material-card
