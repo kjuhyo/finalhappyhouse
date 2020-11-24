692c220f60318d566944f49201ca929d0dfc6ff9
seunghwan <lovelyunsh@naver.com>
2020-11-24 13:56:49 +0900

    123

--- vuetify-material-dashboard-master/src/views/dashboard/tables/RegularTables.vue
+++ vuetify-material-dashboard-master/src/views/dashboard/tables/RegularTables.vue
@@ -8,10 +8,48 @@
       heading="Simple Tables"
       link="components/simple-tables"
     />
+  <v-container>
+    <v-row>
+      <v-col cols = "7"></v-col>
+      <v-col><v-select
+          :items="items"
+          label="시도"
+        ></v-select></v-col>
+      <v-col><v-select
+          :items="items"
+          label="구군"
+        ></v-select></v-col>
+      <v-col><v-select
+          :items="items"
+          label="동"
+        ></v-select></v-col>
+      <v-col>  <v-text-field
+      :label="$t('search')"
+      color="secondary"
+      hide-details
+      style="max-width: 165px;"
+    >
+      <template
+        v-if="$vuetify.breakpoint.mdAndUp"
+        v-slot:append-outer
+      >
+        <v-btn
+          class="mt-n2"
+          elevation="1"
+          fab
+          small
+        >
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </template>
+    </v-text-field>
+     </v-col>
+        </v-row>
+  </v-container>
 
     <base-material-card
       icon="mdi-clipboard-text"
-      title="Simple Table"
+      title="아파트 거래정보"
       class="px-5 py-3"
     >
       <v-simple-table>
@@ -99,90 +137,5 @@
       </v-simple-table>
     </base-material-card>
 
-    <div class="py-3" />
-
-    <base-material-card
-      color="success"
-      dark
-      icon="mdi-clipboard-plus"
-      title="Table on Dark Background"
-      class="px-5 py-3"
-    >
-      <v-simple-table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Country</th>
-            <th>City</th>
-            <th class="text-right">
-              Salary
-            </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Dakota Rice</td>
-            <td>Niger</td>
-            <td>Oud-Turnhout</td>
-            <td class="text-right">
-              $36,738
-            </td>
-          </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Minverva Hooper</td>
-            <td>Curaçao</td>
-            <td>Sinaas-Waas</td>
-            <td class="text-right">
-              $23,789
-            </td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Sage Rodriguez</td>
-            <td>Netherlands</td>
-            <td>Baileux</td>
-            <td class="text-right">
-              $56,142
-            </td>
-          </tr>
-
-          <tr>
-            <td>4</td>
-            <td>Philip Chaney</td>
-            <td>Korea, South</td>
-            <td>Overland Park</td>
-            <td class="text-right">
-              $38,735
-            </td>
-          </tr>
-
-          <tr>
-            <td>5</td>
-            <td>Doris Greene</td>
-            <td>Malawi</td>
-            <td>Feldkirchen in Kärnten</td>
-            <td class="text-right">
-              $63,542
-            </td>
-          </tr>
-
-          <tr>
-            <td>6</td>
-            <td>Mason Porter</td>
-            <td>Chile</td>
-            <td>Gloucester</td>
-            <td class="text-right">
-              $78,615
-            </td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-    </base-material-card>
   </v-container>
 </template>
