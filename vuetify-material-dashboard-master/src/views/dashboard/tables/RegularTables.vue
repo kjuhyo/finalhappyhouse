90db27741b27d5711f77fb2dbf29c307e0627de0
kjuhyo <dkrlwngy@gmail.com>
2020-11-24 18:15:10 +0900

    title change

--- vuetify-material-dashboard-master/src/views/dashboard/tables/RegularTables.vue
+++ vuetify-material-dashboard-master/src/views/dashboard/tables/RegularTables.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container id="regular-tables" fluid tag="section">
-    <base-v-component heading="Simple Tables" link="components/simple-tables" />
+    <base-v-component heading="" link="components/simple-tables" />
     <v-container>
       <v-row>
         <v-col cols="7"></v-col>
